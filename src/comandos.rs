56a47c2d042ddb1215f67fef222191413ee71dd5
CrawKatt <man849916@gmail.com>
2023-07-07 22:53:18 -0400

    Removido: Código experimental comentado

--- src/comandos.rs
+++ src/comandos.rs
@@ -138,46 +138,6 @@ async fn skip(ctx: &Context, msg: &Message) -> CommandResult {
 
     Ok(())
 }
-/*
-#[command]
-#[only_in(guilds)]
-async fn pause(ctx: &Context, msg: &Message) -> CommandResult {
-    let guild = msg.guild(&ctx.cache).unwrap();
-    let guild_id = guild.id;
-
-    let manager = songbird::get(ctx)
-        .await
-        .expect("Songbird Voice client placed in at initialisation.")
-        .clone();
-
-    if let Some(handler_lock) = manager.get(guild_id) {
-        let handler = handler_lock.lock().await;
-        let queue = handler.queue();
-        let _ = queue.pause();
-
-        msg.channel_id
-            .send_message(&ctx.http, |m| {
-                m.embed(|e| {
-                    e.colour(0xa6e3a1)
-                        .title(":pause_button: Paused!")
-                        .timestamp(Timestamp::now())
-                })
-            })
-            .await?;
-    } else {
-        msg.channel_id
-            .send_message(&ctx.http, |m| {
-                m.embed(|e| {
-                    e.colour(0xf38ba8)
-                        .title(":warning: Not in a voice channel.")
-                        .timestamp(Timestamp::now())
-                })
-            })
-            .await?;
-    }
-    Ok(())
-}
-*/
 
 #[command]
 #[only_in(guilds)]
