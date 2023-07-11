c2c8f32f22c453761338c3b65bec3e65a3bf8ff0
CrawKatt <man849916@gmail.com>
2023-07-11 18:46:38 -0400

    Cambios: Se ha creado un módulo error.rs para el error handler y se han movido los imports restantes a dependencies.rs

---
+++ src/utils/error.rs
@@ -0,0 +1,18 @@
+use super::dependencies::*;
+
+pub async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
+    // This is our custom error handler
+    // They are many errors that can occur, so we only handle the ones we want to customize
+    // and forward the rest to the default handler
+    match error {
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx } => {
+            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                println!("Error while handling error: {}", e)
+            }
+        }
+    }
+}
