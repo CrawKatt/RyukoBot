523851d91781a6348054876e2bb73346cbd0cbde
CrawKatt <man849916@gmail.com>
2023-07-11 20:28:33 -0400

    Cambios: Se movió la función autocomplete_concept al módulo dependencies.rs

--- src/utils/dependencies.rs
+++ src/utils/dependencies.rs
@@ -86,3 +86,24 @@ pub async fn autocomplete_status<'a>(
         .filter(move |name| futures::future::ready(name.starts_with(partial)))
         .map(|name| name.to_string())
 }
+
+pub async fn autocomplete_concepts<'a>(
+    _ctx: Context<'_>,
+    partial: &'a str,
+) -> impl Stream<Item = String> + 'a {
+    let Ok(docs) = std::fs::read_dir("docs") else {
+        panic!("Docs not found required!!!");
+    };
+
+    let mut files = vec![];
+    for entry in docs.into_iter() {
+        let entry = entry.unwrap();
+        let filename = entry.file_name().into_string().unwrap();
+        let name = filename.split('.').next().unwrap();
+        files.push(name.to_string());
+    }
+
+
+    futures::stream::iter(files)
+        .filter(move |data: &String| futures::future::ready(data.starts_with(partial)))
+}
