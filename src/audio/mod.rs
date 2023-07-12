614fe0b6a400a5fc62c1b5de9f41bde851c391e8
CrawKatt <man849916@gmail.com>
2023-07-11 21:21:42 -0400

    Cambios: Se han añadido los comandos pause, resume, stop y skip y se han movido a sus propios módulos junto con la funcion join

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -1,2 +1,15 @@
 pub mod play;
+pub mod pause;
+pub mod resume;
+pub mod skip;
+pub mod stop;
+pub mod leave;
+pub mod join;
+
 pub use play::*;
+pub use pause::*;
+pub use resume::*;
+pub use skip::*;
+pub use stop::*;
+pub use leave::*;
+pub use join::*;
