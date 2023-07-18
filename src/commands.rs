27686c91d46c53a5909364bcaf275ca464b7c3c6
CrawKatt <man849916@gmail.com>
2023-07-18 17:31:46 -0400

    Cambios: Añadidos comandos de prueba para uso de botones TODO: hacer que el comando help funcione con botones y tenga un menú de configuraciones

--- src/commands.rs
+++ src/commands.rs
@@ -1,3 +1,4 @@
+use poise::serenity_prelude;
 pub use crate::utils::dependencies::*;
 
 /// Show this help menu
@@ -38,3 +39,51 @@ pub async fn rust(
     Ok(())
 }
 
+#[poise::command(prefix_command, slash_command)]
+pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
+    let pages = &[
+        "Página 1",
+        "Página 2",
+        "Página 3",
+        "Página 4",
+    ];
+
+    poise::builtins::paginate(ctx, pages).await?;
+
+    Ok(())
+}
+
+#[poise::command(slash_command, prefix_command, reuse_response)]
+pub async fn test(ctx: Context<'_>) -> Result<(), Error> {
+    let image_url = "https://raw.githubusercontent.com/serenity-rs/serenity/current/logo.png";
+    ctx.send(|b| {
+        b.content("message 1")
+            .embed(|b| b.description("embed 1").image(image_url))
+            .components(|b| {
+                b.create_action_row(|b| {
+                    b.create_button(|b| {
+                        b.label("button 1")
+                            .style(serenity_prelude::ButtonStyle::Primary)
+                            .custom_id(1)
+                    });
+                    b.create_button(|b| {
+                        b.label("button 2")
+                            .style(serenity_prelude::ButtonStyle::Danger)
+                            .custom_id(2)
+                    });
+                    b.create_button(|b| {
+                        b.label("button 3")
+                            .style(serenity_prelude::ButtonStyle::Secondary)
+                            .custom_id(3)
+                    });
+                    b.create_button(|b| {
+                        b.label("button 4")
+                            .style(serenity_prelude::ButtonStyle::Success)
+                            .custom_id(4)
+                    })
+                })
+            })
+    }).await?;
+
+    Ok(())
+}
