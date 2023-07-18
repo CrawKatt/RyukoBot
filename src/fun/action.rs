5d13d27968834fd3873c856ca9e69b50552740f7
CrawKatt <man849916@gmail.com>
2023-07-18 17:29:34 -0400

    Cambios: Migración de Giphy a uso de API y crate nekosbest para el envío de gifs aleatorios

--- src/fun/action.rs
+++ src/fun/action.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use crate::utils::dependencies::*;
 
 /// Interact with other users with a gif
@@ -6,48 +7,43 @@ use crate::utils::dependencies::*;
 /// ```
 /// /interact greet @user
 /// ```
-#[poise::command(prefix_command, track_edits, aliases("votes"), slash_command)]
+#[poise::command(prefix_command, track_edits, slash_command)]
 pub async fn interact(
     ctx: Context<'_>,
     #[autocomplete = "autocomplete_actions"]
     #[description = "action to do with gif"]
     action: String,
 
-    #[description = "@username"] user: User,
-) -> Result<(), Error> {
-    let url = format!("https://api.giphy.com/v1/gifs/search?api_key=76BcX0eFN6wWFLz3P5CpCj7al8AcWWOK&q={action}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
-    let resp = reqwest::get(url)
-        .await?
-        .json::<ResponseGiphy>()
-        .await?;
+    #[description = "@username"]
+    user: User,
 
+) -> CommandResult {
+
+    let random_gif = nekosbest::get(nekosbest::Category::from_str(&action).unwrap()).await?.url;
     let random_color: u32 = random::<u32>() % 0xFFFFFF;
-
-    let mut rng : StdRng = SeedableRng::from_entropy();
-    let index_random = rng.gen_range(0..resp.data.len());
-
-    let random_gif = resp.data.get(index_random);
-
     let msg = match action.as_str() {
-        "greet" => format!("{} saluda a {}", ctx.author(), user),
         "kiss" => format!("{} beso a {}", ctx.author(), user),
-        "slap" => format!("{} cachetea a {}", ctx.author(), user),
+        "hug" => format!("{} abrazo a {}", ctx.author(), user),
+        "pat" => format!("{} acaricio a {}", ctx.author(), user),
+        "slap" => format!("{} cacheteo a {}", ctx.author(), user),
         "kick" => format!("{} pateo a {}", ctx.author(), user),
         "punch" => format!("{} le dio un puñetazo a {}", ctx.author(), user),
-        "gun" => format!("{} le disparo a {}", ctx.author(), user),
-        _ => format!("{} {} {}", ctx.author(), action, user),
+        "shoot" => format!("{} le disparo a {}", ctx.author(), user),
+        _ => {
+            println!("{} no es una categoria valida", action.clone());
+            return Ok(())
+        }
     };
 
     ctx.send(|f| f
         .embed(|f| f
             .color(random_color)
             .description(msg)
-            .image(random_gif.unwrap().images.original.url.as_str())
+            .image(random_gif)
         )
     ).await?;
 
     Ok(())
-
 }
 
 /// Show help docs for learning rust
@@ -60,32 +56,26 @@ pub async fn act(
     #[description = "status to act"] action: String,
 ) -> Result<(), Error> {
 
-    let url = format!("https://api.giphy.com/v1/gifs/search?api_key=76BcX0eFN6wWFLz3P5CpCj7al8AcWWOK&q={action}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
-    let resp = reqwest::get(url)
-        .await?
-        .json::<ResponseGiphy>()
-        .await?;
-
     let random_color: u32 = random::<u32>() % 0xFFFFFF;
-
-    let index_random = rand::thread_rng().gen_range(0..resp.data.len());
-
-    let random_gif = resp.data.get(index_random);
+    let random_gif = nekosbest::get(nekosbest::Category::from_str(&action).unwrap()).await?.url;
 
     let msg = match action.as_str() {
-        "greet" => format!("{} saluda a todos", ctx.author()),
-        "playing" => format!("{} esta jugando", ctx.author()),
-        "angry" => format!("{} esta enojado", ctx.author()),
-        "happy" => format!("{} esta feliz", ctx.author()),
-        "boring" => format!("{} esta aburrido", ctx.author()),
-        _ => format!("{} esta {}",  ctx.author(), action),
+        "blush" => format!("{} Se sonroja", ctx.author()),
+        "bored" => format!("{} Esta aburrido", ctx.author()),
+        "cry" => format!("{} Esta llorando", ctx.author()),
+        "dance" => format!("{} Esta bailando", ctx.author()),
+        "happy" => format!("{} Esta feliz", ctx.author()),
+        _ => {
+            println!("{} no es una categoría valida", action.clone());
+            return Ok(())
+        }
     };
 
     ctx.send(|f| f
         .content(msg)
         .embed(|f| f
             .color(random_color)
-            .image(random_gif.unwrap().images.original.url.as_str())
+            .image(random_gif)
         )
     ).await?;
 
